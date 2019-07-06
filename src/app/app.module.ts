65837a3469c560161a3b75babf8f8e348557bd50
Matt Pavlovich <meacreatio@gmail.com>
2019-07-06 18:02:40 +0200

    add passing value from parent to child

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BindClassToHTMLComponent } from './bind-class-to-html/bind-class-to-html.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BindClassToHTMLComponent
+    BindClassToHTMLComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
